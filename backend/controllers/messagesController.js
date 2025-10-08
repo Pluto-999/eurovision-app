cd08d53dd127071cf5f2b9657c628c415d394c78
Pluto-999 <adamw0909@gmail.com>
2025-10-08 12:12:48 +0100

    Improve frontend of chat and add profile picture fetching from backend

--- backend/controllers/messagesController.js
+++ backend/controllers/messagesController.js
@@ -1,6 +1,5 @@
 const sql = require("../config/db")
 const asyncWrapper = require("../middleware/asyncWrapper")
-const { io } = require("../socket")
 
 const getMessages = asyncWrapper(async (req, res) => {
     const currentUser = req.username
@@ -8,6 +7,18 @@ const getMessages = asyncWrapper(async (req, res) => {
 
     if (currentUser === otherUser) {
         return res.status(404).json({ success: false, message: "Cannot get messages sent to yourself" })
+    }
+
+    const otherUserDetails = await sql`
+        SELECT username, profile_picture
+        FROM friends INNER JOIN users
+        ON (friends.user1=${currentUser} AND users.username=friends.user2)
+        OR (friends.user2=${currentUser} AND users.username=friends.user1)
+        WHERE users.username=${otherUser}
+    `
+
+    if (otherUserDetails.length === 0) {
+        return res.status(404).json({ success: false, message: `You cannot chat with ${otherUser}, as they either don't exist, or you aren't currently friends with them` })
     }
 
     const currentUserSentMessages = await sql`
@@ -28,7 +39,8 @@ const getMessages = asyncWrapper(async (req, res) => {
 
     res.status(200).json({ 
         success: true, 
-        messages: allMessages 
+        messages: allMessages,
+        otherUserDetails: otherUserDetails[0]
     })
 })
 
--- frontend/src/pages/friends_pages/Chat.jsx
+++ frontend/src/pages/friends_pages/Chat.jsx
@@ -1,14 +1,27 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import socket from "../../socket"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import axios from "axios"
 import { IoSend } from "react-icons/io5"
 import "../../styles/chat.css"
+import toast from "react-hot-toast"
+import { Ring } from "ldrs/react"
+import "ldrs/react/Ring.css"
+import { useUserContext } from "../../context/userContext"
 
 function Chat() {
     const [message, setMessage] = useState("")
     const params = useParams()
     const [allMessages, setAllMessages] = useState([])
+    const [userDetails, setUserDetails] = useState("")
+    const navigate = useNavigate()
+    const [loading, setLoading] = useState(false)
+    const messagesEndRef = useRef(null)
+    const { user } = useUserContext()
+
+    useEffect(() => {
+        messagesEndRef.current.scrollIntoView({ behavior: "smooth" })
+    }, [allMessages])
 
     const sendMessage = () => {
         socket.emit("sendMessage", {
@@ -18,7 +31,33 @@ function Chat() {
         setMessage("")
     }
 
+    const fetchMessages = async () => {
+        setLoading(true)
+        try {
+            const response = await axios.get(`http://localhost:3000/api/messages/${params.username}`, 
+                { withCredentials: true }
+            )
+            console.log(response)
+            setAllMessages(response.data.messages)
+            setUserDetails(response.data.otherUserDetails)
+        } 
+        catch (error) {
+            if (error.response?.data?.message) {
+                toast.error(error.response.data.message)
+            }
+            else {
+                toast.error("Something went wrong, please try again")
+            }
+            navigate("/user/home")
+        }
+        finally {
+            setLoading(false)
+        }
+    }
+
     useEffect(() => {
+        fetchMessages()
+
         const handleReceiveMessage = (data) => {
             setAllMessages(prev => [...prev, data])
         }
@@ -30,48 +69,71 @@ function Chat() {
         }
     }, [])
 
-    useEffect(() => {
-        axios.get(`http://localhost:3000/api/messages/${params.username}`,
-            { withCredentials: true }
-        )
-        .then(response => {
-            setAllMessages(response.data.messages)
-        })
-    }, [])
-
     return (
-        <div className="whole_page">
-            <h1> Chat with {params.username} </h1>
-            
-            <ul> 
-                {
-                    allMessages.map((message) => (
-                        
-                        <li 
-                            key={message.message_id} 
-                            className={`chat ${message.sender === params.username ? "chat-start" : "chat-end"}`}
-                        >
-                            <div className="chat-bubble"> {message.content} </div>
-                            
-                            <div className="chat-footer"> 
-                                {new Date(message.timestamp).toLocaleString("en-GB", {
-                                    dateStyle: "short",
-                                    timeStyle: "short"
-                                })} 
+        <div className="chat_page">
+            {loading ? (
+                <div className="loader">
+                    <Ring />
+                </div>
+            ) : (
+                <>
+                    <h1 className="chat_header"> 
+                        <div className="avatar">
+                            <div className="w-15 rounded-full">
+                                <img src={userDetails.profile_picture} />
                             </div>
-                        </li>
-                        
-                    ))
-                } 
-            </ul>
-            <div className="message_bar">
-                <input 
-                    placeholder="message" 
-                    value={message}
-                    onChange={(event) => {setMessage(event.target.value)}}
-                ></input>
-                <button className="btn" onClick={sendMessage}> <IoSend /> </button>
-            </div>
+                        </div>
+                        {userDetails.username} 
+                    </h1>
+                
+                    <div className="min-h-[70vh]"> 
+                        {
+                            allMessages.map((message) => (
+                                
+                                <div 
+                                    key={message.message_id}
+                                    className={`chat ${message.sender === params.username ? "chat-start" : "chat-end"}`}
+                                >
+                                    <div className="chat-image avatar">
+                                        <div className="w-10 rounded-full">
+                                            <img 
+                                                src={message.sender === params.username ? 
+                                                    userDetails.profile_picture : 
+                                                    user.profile_picture
+                                                }
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="chat-header">
+                                        {message.sender === params.username ? userDetails.username : user.username}
+                                    </div>
+                                    <div className="chat-bubble"> {message.content} </div>
+                                    
+                                    <div className="chat-footer"> 
+                                        {new Date(message.timestamp).toLocaleString("en-GB", {
+                                            dateStyle: "short",
+                                            timeStyle: "short"
+                                        })} 
+                                    </div>
+                                </div>
+                                
+                            ))
+                        }
+                        <div ref={messagesEndRef}/>
+                    </div>
+
+                    <div className="message_bar">
+                        <input 
+                            placeholder="Message"
+                            type="text"
+                            value={message}
+                            onChange={(event) => {setMessage(event.target.value)}}
+                            className="input w-8/10"
+                        ></input>
+                        <button className="btn w-1/10" onClick={sendMessage}> <IoSend /> </button>
+                    </div>
+                </>
+            )}
         </div>
     )
 }
