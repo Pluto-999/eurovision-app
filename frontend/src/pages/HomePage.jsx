6f29565922c7f80e89558b3aa541e5fe607b854a
Pluto-999 <adamw0909@gmail.com>
2025-10-14 11:18:53 +0100

    Add improved home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,25 +1,70 @@
+import { FaChrome } from "react-icons/fa6"
+import { FaUserLock } from "react-icons/fa6"
+import { IoChatbubblesOutline } from "react-icons/io5"
+import { FaUserFriends } from "react-icons/fa"
+import { IoSearch } from "react-icons/io5"
+import { FaRankingStar } from "react-icons/fa6"
+import { IoLogoGithub } from "react-icons/io5"
 
 function HomePage() {
   return (
-      <div className="mx-auto max-w-3xl px-4">
-        <h1>Welcome to Euroscore</h1>
+      <div className="flex flex-col gap-20">
+        <div className="flex flex-col items-center gap-3">
+          <h1 className="font-medium">Welcome to Euroscore</h1>
+          <div> View official Eurovision results from 2021 onwards, rank and rate entries each year to shape community rankings, and share your results with friends and groups to compare favorites and see who tops your group's leaderboard.</div>
+        </div>
 
-        <p>
-          Euroscore is a Eurovision-themed web application built by Adam Wood as a learning project to deepen my understanding of full-stack development. This project uses <strong>Node.js</strong>, <strong>Express</strong> and <strong>PostgreSQL</strong> for the backend, and <strong>React</strong> for the frontend.
-        </p>
+        <div className="flex flex-col gap-7">
+          <h2 className="bg-[#646cff] rounded-3xl text-white shadow-lg self-center"> FEATURES </h2>
+          <div className="grid grid-cols-3">
+            <div className="flex flex-col gap-5">
+              <div className="bg-[#c7d2fe] w-fit p-3 rounded-4xl shadow-lg">
+              <FaChrome size={30} />
+              </div>
+              Automated web scraping of official Eurovision entries and results (2021 onwards) from Wikipedia.
+            </div>
+            <div className="flex flex-col gap-5">
+              <div className="bg-[#c7d2fe] w-fit p-3 rounded-4xl shadow-lg">
+              <FaUserLock size={30} />
+              </div>
+              Secure authentication via JWTs and cookies, as well as reset password functionality through emails.
+            </div>
+            <div className="flex flex-col gap-5">
+              <div className="bg-[#c7d2fe] w-fit p-3 rounded-4xl shadow-lg">
+              <IoSearch size={30} />
+              </div>
+              Search for any Eurovision entry from 2021 onwards, based on country, artist, year or song name.
+            </div>
+            <div className="flex flex-col gap-5">
+              <div className="bg-[#c7d2fe] w-fit p-3 rounded-4xl shadow-lg">
+              <FaRankingStar size={30} />
+              </div>
+              View official results as well as community results from every user's submitted rankings.
+            </div>
+            <div className="flex flex-col gap-5">
+              <div className="bg-[#c7d2fe] w-fit p-3 rounded-4xl shadow-lg">
+              <FaUserFriends size={30} />
+              </div>
+              Add friends to share your rankings and ratings of entries.
+            </div>
+            <div className="flex flex-col gap-5">
+              <div className="bg-[#c7d2fe] w-fit p-3 rounded-4xl shadow-lg">
+              <IoChatbubblesOutline size={30} />
+              </div>
+              Real-time user chat via Socket.io.
+            </div>
+          </div>
+        </div>
 
-        <h2>Key features include:</h2>
-        <ul className="list-disc pl-6 mb-6">
-          <li>Automated scraping of official Eurovision results (2021 onward) from Wikipedia</li>
-          <li>Secure authentication using JWTs and cookies as well as reset password functionality through emails</li>
-          <li>Real-time user chat via WebSockets</li>
-          <li>Community rankings and voting</li>
-          <li>Detailed viewing of official and user-submitted results</li>
-        </ul>
-
-        <p className="text-sm">
-          ⚠️ Note: This project primarily served as a technical learning experience, so UI/UX design was not the main focus. Visual improvements may be made in future updates.
-        </p>
+        <div className="flex justify-center">
+          <a
+            href="https://github.com/Pluto-999/eurovision-app"
+            className="flex p-3 gap-3 self-center text-lg items-center bg-[#646cff] text-white w-fit rounded-4xl shadow-lg"
+          >
+            <IoLogoGithub size={50} color="white"/>
+            <div> Github Repository </div>
+          </a>
+        </div>
       </div>
   );
 }
