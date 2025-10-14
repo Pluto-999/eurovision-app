b0a065ef4299902fea03c1a7a7228dd7af9a9457
Pluto-999 <adamw0909@gmail.com>
2025-10-14 12:09:40 +0100

    Add link styles to account home page

--- frontend/src/pages/auth_pages/AccountHomePage.jsx
+++ frontend/src/pages/auth_pages/AccountHomePage.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 import "ldrs/react/Ring.css"
 import { useUserContext } from "../../context/userContext"
+import "../../styles/links.css"
 
 function HomePage () {
     const { user } = useUserContext()
@@ -11,12 +12,12 @@ function HomePage () {
             { user ? (
                 <div className="flex flex-col gap-3">
                     <div> You are already logged in as {user.username}</div>
-                    <div><Link to="/user/home"> Go to your dashboard </Link></div>
+                    <div><Link to="/user/home" className="text_link"> Go to your dashboard </Link></div>
                 </div>
             ) : (
                 <div className="flex flex-col gap-3">
-                    <div><Link to="/account/register"> Register </Link></div>
-                    <div><Link to="/account/login"> Login </Link></div>
+                    <div><Link to="/account/register" className="text_link"> Register </Link></div>
+                    <div><Link to="/account/login" className="text_link"> Login </Link></div>
                 </div>
             )}
             
